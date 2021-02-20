468b314c97b915eaa5f572f99a5b2955be7b8622
kanzff <rivakanzf@gmail.com>
2021-02-20 14:45:21 +0700

    fix port

--- server/services/movies/app.js
+++ server/services/movies/app.js
@@ -7,7 +7,7 @@ const { connect, getDatabase } = require('./config/mongodb')
 const { MovieController } = require('./controllers/movieControllers')
 const movieRouter = require('./routes/movieRoutes')
 const app = express()
-const port = 4001
+const port = process.env.PORT || 4001
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
