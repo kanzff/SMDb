437a07001b27a15d2e450709452bbe63a14490d1
kanzff <rivakanzf@gmail.com>
2021-02-27 23:33:55 +0700

    deploy

--- server/orchestrator/app.js
+++ server/orchestrator/app.js
@@ -1,7 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server')
 const axios = require('axios')
-const Redis = require('ioredis')
-const redis = new Redis()
+// const Redis = require('ioredis')
+// const redis = new Redis()
 
 const typeDefs = gql`
   type Movie {
@@ -56,29 +56,29 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     getMovies: async () => {
-      const movieData = await redis.get('movies:data')
-      if (movieData) {
-        // console.log('dari redis', movieData)
-        return JSON.parse(movieData)
-      } else {
+      // const movieData = await redis.get('movies:data')
+      // if (movieData) {
+      //   // console.log('dari redis', movieData)
+      //   return JSON.parse(movieData)
+      // } else {
         return axios({
-          url: 'http://localhost:4001/movies',
+          url: 'https://entertainme-service-movie.herokuapp.com/movies',
           method: 'GET'
         })
           .then(({data}) => {
             // console.log('bukan dari redis')
-            redis.set('movies:data', JSON.stringify(data))
+            // redis.set('movies:data', JSON.stringify(data))
             return data
           })
           .catch(err => {
             console.log(err)
           })
-      }
+      // }
     },
     getMovie: (parent, args, context, info) => {
       const { _id } = args
       return axios({
-        url: `http://localhost:4001/movies/${_id}`,
+        url: `https://entertainme-service-movie.herokuapp.com/movies/${_id}`,
         method: 'GET'
       })
         .then(({data}) => {
@@ -90,29 +90,29 @@ const resolvers = {
         })
     },
     getSeries: async () => {
-      const seriesData = await redis.get('series:data')
-      if (seriesData) {
-        // console.log('dari redis', movieData)
-        return JSON.parse(seriesData)
-      } else {
+      // const seriesData = await redis.get('series:data')
+      // if (seriesData) {
+      //   // console.log('dari redis', movieData)
+      //   return JSON.parse(seriesData)
+      // } else {
         return axios({
-          url: 'http://localhost:4002/series',
+          url: 'https://entertainme-service-series.herokuapp.com/series',
           method: 'GET'
         })
           .then(({data}) => {
             // console.log('bukan dari redis')
-            redis.set('series:data', JSON.stringify(data))
+            // redis.set('series:data', JSON.stringify(data))
             return data
           })
           .catch(err => {
             console.log(err)
           })
-      }
+      // }
     },
     getOneSeries: (parent, args, context, info) => {
       const { _id } = args
       return axios({
-        url: `http://localhost:4002/series/${_id}`,
+        url: `https://entertainme-service-series.herokuapp.com/series/${_id}`,
         method: 'GET'
       })
         .then(({data}) => {
@@ -127,10 +127,10 @@ const resolvers = {
   Mutation: {
     addMovie: async (parent, args, context, info) => {
       const { title, overview, poster_path, popularity, tags } = args.data
-      await redis.del('movies:data')
+      // await redis.del('movies:data')
 
       return axios({
-        url: 'http://localhost:4001/movies',
+        url: 'https://entertainme-service-movie.herokuapp.com/movies',
         method: 'POST',
         data: {
           title,
@@ -149,10 +149,10 @@ const resolvers = {
     },
     updateMovie: async (parent, args, context, info) => {
       const { _id, title, overview, poster_path, popularity, tags } = args.data
-      await redis.del('movies:data')
+      // await redis.del('movies:data')
 
       return axios({
-        url: `http://localhost:4001/movies/${_id}`,
+        url: `https://entertainme-service-movie.herokuapp.com/movies/${_id}`,
         method: 'PUT',
         data: {
           title,
@@ -171,10 +171,10 @@ const resolvers = {
     },
     destroyMovie: async (parent, args, context, info) => {
       const { _id } = args
-      await redis.del('movies:data')
+      // await redis.del('movies:data')
 
       return axios({
-        url: `http://localhost:4001/movies/${_id}`,
+        url: `https://entertainme-service-movie.herokuapp.com/movies/${_id}`,
         method: 'DELETE'
       })
         .then(({data}) => {
@@ -187,8 +187,13 @@ const resolvers = {
   }
 }
 
-const server = new ApolloServer({ typeDefs, resolvers})
+const server = new ApolloServer({ 
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true, 
+})
 
-server.listen().then(({url}) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({url}) => {
   console.log('server ready at ' + url)
 })
